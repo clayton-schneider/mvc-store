f66c96cfcdb5bd147693a189b0157412ccbcad1b
Clayton <schneiderc2016@gmail.com>
2021-06-28 16:46:40 -0400

    centered videos

--- src/components/ViewProduct.vue
+++ src/components/ViewProduct.vue
@@ -7,7 +7,7 @@
   >
     <v-card v-if="viewProduct.data">
       <v-toolbar dark color="primary">
-        <v-btn icon dark @click="TOGGLE_LISCENCE_LOG"
+        <v-btn icon dark @click="TOGGLE_VIEW_PROD"
           ><v-icon>mdi-close</v-icon></v-btn
         >
         <v-toolbar-title>{{ viewProduct.data.name }}</v-toolbar-title>
@@ -16,7 +16,13 @@
           <v-btn text dark @click="TOGGLE_LISCENCE_LOG">Liscence</v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <v-container v-html="viewProduct.data.code"> </v-container>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" md="8">
+            <div v-html="viewProduct.data.code"></div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-card>
     <LiscenceDialog />
   </v-dialog>
@@ -34,7 +40,7 @@ export default {
   },
   computed: mapGetters(['isViewProdOpen', 'viewProduct']),
   methods: {
-    ...mapActions(['TOGGLE_LISCENCE_LOG']),
+    ...mapActions(['TOGGLE_LISCENCE_LOG', 'TOGGLE_VIEW_PROD']),
   },
 };
 </script>
