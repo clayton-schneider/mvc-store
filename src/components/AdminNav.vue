7e27b5db0daca6c66495361054f1268422d1a04b
Clayton <schneiderc2016@gmail.com>
2021-07-01 14:26:31 -0400

    admin nav only on admin user

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -1,17 +1,30 @@
 <template>
-  <v-toolbar>
-    <v-toolbar-title><router-link to="/">MVC</router-link></v-toolbar-title>
+  <v-toolbar v-if="user.profileInfo.type === 'admin'">
+    <v-toolbar-title
+      ><router-link to="/"
+        ><span class="black--text">Media</span> Voices for
+        <span class="orange--text text--darken-3">Children</span></router-link
+      ></v-toolbar-title
+    >
     <v-spacer></v-spacer>
-    <v-btn :to="{ name: 'ManageProducts' }" class="mr-2">Manage Products</v-btn>
-    <v-btn :to="{ name: 'ManageUsers' }" class="mr-2">Manage Users</v-btn>
-    <v-btn @click="logout">Logout</v-btn>
+    <v-toolbar-items>
+      <v-btn :to="{ name: 'ManageProducts' }" class="mr-2" text
+        >Manage Products</v-btn
+      >
+      <v-btn :to="{ name: 'ManageUsers' }" class="mr-2" text
+        >Manage Users</v-btn
+      >
+      <v-btn @click="logout" text>Logout</v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import { projectAuth } from '../firebase/config';
 export default {
   name: 'AdminNav',
+  computed: mapGetters(['user']),
   methods: {
     async logout() {
       const res = await projectAuth.signOut();
@@ -20,3 +33,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.v-toolbar__title a {
+  color: #616161;
+  text-decoration: none;
+  font-weight: bold;
+}
+</style>
